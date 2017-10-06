c7a7640c1e82d6a933d46dfc4254630f6b8780d9
Unknown <jnguy142@ucr.edu>
2017-10-06 14:04:17 -0700

    implemented progress bar for ratings

--- client/src/components/Starsview.js
+++ client/src/components/Starsview.js
@@ -6,11 +6,11 @@ var Starsview = (props) => {
     <div id="stars-view">
       {props.avg} STARS
       <div>
-        1 Stars: <ProgressBar striped bsStyle="success" now={40} />
-        2 Stars: <ProgressBar striped bsStyle="info" now={20} />
-        3 Stars: <ProgressBar striped bsStyle="warning" now={60} />
-        4 Stars: <ProgressBar striped bsStyle="warning" now={60} />
-        5 Stars: <ProgressBar striped bsStyle="danger" now={80} />
+        1 Stars: <ProgressBar striped now={40} />
+        2 Stars: <ProgressBar striped now={20} />
+        3 Stars: <ProgressBar striped now={60} />
+        4 Stars: <ProgressBar striped now={60} />
+        5 Stars: <ProgressBar striped now={80} />
       </div>
     </div>
   )
