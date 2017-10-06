8bbffaaf8b8c9e65ec2817d70c293df09ed070a0
Unknown <jnguy142@ucr.edu>
2017-10-05 19:00:17 -0700

    handles submitting comment without refreshing

--- client/src/components/Reviewform.js
+++ client/src/components/Reviewform.js
@@ -5,11 +5,19 @@ class Reviewform extends React.Component {
     super(props);
   }
 
+  onClickHandler (event) {
+    var comment = document.getElementById("user-review").value;
+    document.getElementById("user-review").value = "";
+    console.log(comment);
+    event.preventDefault();
+  }
+
   render () {
     return(
       <form id="user-review-form">
           <textarea id="user-review">
           </textarea>
+          <span><button onClick={this.onClickHandler} id="submit-review">Submit</button></span>
       </form>
       )
   }
